50493fb2cd2934995a4dc625df1836e8b600cbb3
Jordan <jwald3@brockport.edu>
2025-02-12 20:59:32 -0500

    Added basic "generic type" implementation

--- cmd/main.go
+++ cmd/main.go
@@ -5,21 +5,30 @@ import (
 	lollipop "lollipop/internal"
 )
 
+type TripStatus string
+
+const (
+	TripStatusScheduled  TripStatus = "SCHEDULED"
+	TripStatusInProgress TripStatus = "IN_PROGRESS"
+	TripStatusCompleted  TripStatus = "COMPLETED"
+	TripStatusCancelled  TripStatus = "CANCELLED"
+)
+
 func main() {
-	stateMachine := lollipop.NewStateMachine("A")
+	stateMachine := lollipop.NewStateMachine(TripStatusScheduled)
 
-	stateMachine.AddTransition("A", "B")
-	stateMachine.AddTransition("B", "C")
-	stateMachine.AddTransition("C", "A")
+	stateMachine.AddTransition(TripStatusScheduled, TripStatusInProgress)
+	stateMachine.AddTransition(TripStatusScheduled, TripStatusCancelled)
+	stateMachine.AddTransition(TripStatusInProgress, TripStatusCompleted)
 
-	fmt.Printf("Initial state: %s\n", stateMachine.State)
+	fmt.Printf("Initial state: %v\n", stateMachine.State)
 
-	if err := stateMachine.Transition("B"); err != nil {
+	if err := stateMachine.Transition(TripStatusInProgress); err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 	fmt.Printf("State after transition: %s\n", stateMachine.State)
 
-	if err := stateMachine.Transition("A"); err != nil {
+	if err := stateMachine.Transition(TripStatusCancelled); err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 }
