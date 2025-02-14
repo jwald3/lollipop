2fa64c63ce0777552af499314f1aa66f81f3c338
Jordan Riesel <jordan.riesel@gmail.com>
2025-02-13 19:15:50 -0500

    Added descriptive comments

--- cmd/main.go
+++ cmd/main.go
@@ -15,9 +15,11 @@ const (
 func main() {
 	sm := lollipop.NewStateMachine(Off)
 
+	// register valid state paths
 	sm.AddTransition(Off, On)
 	sm.AddTransition(On, Off)
 
+	// register entry actions for the available states
 	sm.SetEntryAction(On, func() error {
 		fmt.Println("Light bulb warming up...")
 		return nil
@@ -30,7 +32,10 @@ func main() {
 
 	fmt.Printf("Current state: %v\n", sm.State)
 
+	// perform transactions with entry actions attached
 	fmt.Println("Turning light on...")
+	// if the transition is valid, the state machine will call the entry action
+	// associated with entering the "On" state
 	if err := sm.Transition(On); err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
